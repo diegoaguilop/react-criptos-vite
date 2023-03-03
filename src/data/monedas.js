fa0bc298e7db90b4c34012a2dca24764138510e9
diegoaguilop <diego.aguilar1811@gmail.com>
2023-03-03 15:41:33 +0100

    Proyecto Finalizado

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+    {id: "EUR", nombre: "Euro de España"},
+    {id: "USD", nombre: "Dolar de Estados Unidos"},
+    {id: "GBP", nombre: "Libra esterlina de Inglaterra"},
+    {id: "JPY", nombre: "Yen de Japon"},
+    {id: "CHF", nombre: "Franco de Suiza"}
+]
+
+export {
+    monedas
+}
